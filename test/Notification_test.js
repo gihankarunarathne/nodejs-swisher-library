214ed8809f6b1122b2c1dc3b00c4f09bbdc52204
Aruna Herath <arunabherath@gmail.com>
2014-09-18 13:39:22 +0530

    Updated the Notification client according to the changed api.
    Added a test.

---
+++ test/Notification_test.js
@@ -0,0 +1,27 @@
+describe('Notification swisher client', function(){
+  var notifications = require('../lib/Notification');
+  var assert = require('assert');
+  var testConf = require('./config_test.json');
+
+  var notifService;
+
+  before(function (done) {
+    notifService = notifications.Notification(testConf.testAppSecret, testConf.testAppId, {
+      grantType: "access_token",
+      scope: ['Notification']
+    });
+    console.log("Start Tests");
+    done();
+  });
+
+  it('Should successfully trigger events', function(done){
+    notifService.trigger('myChannel', 'myEvent', 'my message', function(err, data){
+      console.log(err)
+
+      if(!err){
+        assert.equal(data.msg, "Success");
+        done();
+      }
+    });
+  });
+});
